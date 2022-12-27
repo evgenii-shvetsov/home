c96ecdbc74698a73127f1872d5fde696c83442a1
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-27 10:39:07 -0800

    add reset css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch, Redirect} from 'react-router-dom';
+import './index.css'
 
 import Homepage from './components/Homepage';
 import Navigation from './components/Navigation';
