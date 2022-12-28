0556468c6265a245223c68623ee3b2b2994a7232
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-28 11:46:55 -0800

    add listing index page

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Homepage.css"
+import ListingIndexPage from '../ListingIndexPage'
+
 const Homepage = () => {
   return (
     <main>
       <section className='search-bar'></section>
-      <section className='listings-cards'></section>
+      <section className='listings-cards'><ListingIndexPage/></section>
       <section className='deal-types'></section>
     </main>
   )
---
+++ frontend/src/components/ListingIndexPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListingIndexPage = () => {
+  return (
+    <div>Listing Index Page</div>
+  )
+}
+
+export default ListingIndexPage
