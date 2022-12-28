9da61e7a17301cf341cff37d388a52f5906f80b3
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-27 17:16:43 -0800

    fix page 404

--- frontend/src/components/Page404/index.js
+++ frontend/src/components/Page404/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Page404.css"
+
 
 const errorPage = () => {
   return (
-    <div>Error 404, page was not found</div>
+    <section>
+      <section className='error404-style'>
+        <div className='error-text'>
+          <h1>Uh oh, <br />
+          something broke.
+          </h1>
+          <p>Error 404 - page not found</p>
+          <Link to="/"><button>Return Home</button></Link>
+        </div>
+      </section>
+    </section>
   )
 }
 
