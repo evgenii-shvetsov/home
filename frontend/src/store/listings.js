79fc5c3eb4eb0435284b315afdefab289dbbd1fa
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-28 11:05:44 -0800

    add store listings

---
+++ frontend/src/store/listings.js
@@ -0,0 +1,67 @@
+import csrfFetch from './csrf';
+
+//Action constants
+const SET_LISTINGS = "listings/setListings";
+const ADD_LISTING = "listings/addListings";
+
+//Action creators
+const setListings = (listings) => ({
+    type: SET_LISTINGS,
+    payload: listings
+});
+
+const addListing = (listing) => ({
+    type: ADD_LISTING,
+    payload: listing
+});
+
+//THUNK ACTION CREATORS
+
+export const fetchListings = () => async (dispatch) =>{
+    const res = await csrfFetch("/api/listings");
+    if(res.ok){
+        const listings = await res.json();
+        dispatch(setListings(listings));
+    }
+};
+
+export const fetchListing = (listingId) => async (dispatch) =>{
+    const res = await csrfFetch(`/api/listings/${listingId}`);
+    if(res.ok){
+        const listing = await res.json();
+        dispatch(addListing(listing));
+    }
+};
+
+export const createBench = (listingData) => async (dispatch) =>{
+    const res = await csrfFetch("/api/listings", {
+        method: "POST",
+        body: JSON.stringify(listingData),
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    });
+    if(res.ok){
+        const listing = await res.json();
+        dispatch(addListing(listing))
+    }
+}
+
+
+////////////////
+//add thunk for EDIT AND DESTROY
+////////////////
+
+const listingReducer = (state = {}, action) => {
+    switch (action.type) {
+        case SET_LISTINGS:
+            return {...state, ...action.payload};
+        case ADD_LISTING:
+            return {...state, ...action.payload};
+        default:
+            return state;
+    }
+};
+
+export default listingReducer;
