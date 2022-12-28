7cc0cbe55c4795c26e23f41d3862fe4420ac655c
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-27 16:23:06 -0800

    add footer details

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import "./Footer.css"
+import logo from '../../assets/home-logo.jpg';
 
 const Footer = () => {
   return (
     <footer>
-        Footer Details
+        <section className='footer-info'>
+            <NavLink exact to="/"><img id="footer-logo" src={logo} alt="company logo" /></NavLink>
+            <p>GitHub</p>
+            <p>LinkedIn</p>
+            
+        </section> 
+        {/* <NavLink exact to="/"><img id="logo" src={logo} alt="company logo" /></NavLink> */}
+        <section className='footer-big-logo'></section>
     </footer>
     
   )
