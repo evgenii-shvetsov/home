0e2d4b9844a2fb01757afc0808510087f67871c7
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-28 11:00:43 -0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,43 +49,43 @@ ApplicationRecord.transaction do
 
     puts "Creating listings..."
 
-    # Listing.create!({
-    #   owner_id: 1,
-    #   status: "active",
-    #   deal_type: "for sale",
-    #   description:  "Great starter home in the Bayview neighboor with 3 beds and 1 bath on upper level - updated kitchen and bathroom, lower level consist one large family room and second bath.",
-    #   zip: 94124,
-    #   state: "CA",
-    #   city: "San Francisco",
-    #   address: "1826 Keith St",
-    #   lat: 37.79352054026415, 
-    #   lng: -122.40412603039728, 
-    #   bedroom: 3,
-    #   bathroom: 2,
-    #   size: 1101,  
-    #   year_built: 1955,  
-    #   price: 1999999,
-    #   listing_type: "house"
-    # })
+    Listing.create!({
+      owner_id: 1,
+      status: "active",
+      deal_type: "for sale",
+      description:  "Great starter home in the Bayview neighboor with 3 beds and 1 bath on upper level - updated kitchen and bathroom, lower level consist one large family room and second bath.",
+      zip: 94124,
+      state: "CA",
+      city: "San Francisco",
+      address: "1826 Keith St",
+      lat: 37.79352054026415, 
+      lng: -122.40412603039728, 
+      bedroom: 3,
+      bathroom: 2,
+      size: 1101,  
+      year_built: 1955,  
+      price: 1999999,
+      listing_type: "house"
+    })
 
-    # Listing.create!({
-    #   owner_id: 2,
-    #   status: "active",
-    #   deal_type: "for sale",
-    #   description:  "An authentic Mid-Century Modern, the Kellogg House is a standout on Potrero Hill. Built in 1948 by F.J. McCarthy, the home was extensively expanded in 2015 on a double-lot near the top of the coveted North Slope with panoramic views of the City skyline.",
-    #   zip: 94107,
-    #   state: "CA",
-    #   city: "San Francisco",
-    #   address: "1740 20th St",
-    #   lat: 37.78456703234154, 
-    #   lng: -122.45184789377329,
-    #   bedroom: 3,
-    #   bathroom: 3,
-    #   size: 2976,  
-    #   year_built: 1950,  
-    #   price: 870000,
-    #   listing_type: "house",
-    # })
+    Listing.create!({
+      owner_id: 2,
+      status: "active",
+      deal_type: "for sale",
+      description:  "An authentic Mid-Century Modern, the Kellogg House is a standout on Potrero Hill. Built in 1948 by F.J. McCarthy, the home was extensively expanded in 2015 on a double-lot near the top of the coveted North Slope with panoramic views of the City skyline.",
+      zip: 94107,
+      state: "CA",
+      city: "San Francisco",
+      address: "1740 20th St",
+      lat: 37.78456703234154, 
+      lng: -122.45184789377329,
+      bedroom: 3,
+      bathroom: 3,
+      size: 2976,  
+      year_built: 1950,  
+      price: 870000,
+      listing_type: "house",
+    })
 
 
     puts "Done!"
