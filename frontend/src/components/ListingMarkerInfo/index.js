311bad329a94f0b5564feb064342f5177d4064a0
evgenii-shvetsov <sea.dev91@gmail.com>
2023-01-30 14:12:39 -0800

    add placeholder for listing without a photo on the map

--- frontend/src/components/ListingMarkerInfo/index.js
+++ frontend/src/components/ListingMarkerInfo/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './ListingMarkerInfo.css'
+import logo from '../../assets/home-logo.png';
+
 const ListingMarkerInfo = ( {info} ) => {
+
+  let listingPhoto = info.listing_photo;
+
   return (
     <div className='listing-marker-info'>
-                <img id="marker-listing-photo" src={info.listing_photo} alt="" />
+                <img id="marker-listing-photo" src={listingPhoto ? listingPhoto : logo} alt="" />
                 <p id="marker-listing-price">${info?.price.toLocaleString()}</p>
                 <p>{info.bedroom} bd {info.bathroom} ba</p>
                 <p>{info.listing_type} for {info.deal_type}</p>
